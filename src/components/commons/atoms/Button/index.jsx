e17600a8ed896b503505bda5e8dd5bfcbd035cf6
kota <kota200016@icloud.com>
2021-08-16 09:26:09 +0900

    アニメーションの名前変更/About部分のページ遷移追加

--- src/components/commons/atoms/Button/index.jsx
+++ src/components/commons/atoms/Button/index.jsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 import styles from "./styles.module.scss";
 
 export function Button() {
     return (
         <div className={styles.link}>
-            <div className={styles.btn}>MORE</div>
+            <Link href="/about">
+                <div className={styles.btn}>MORE</div>
+            </Link>
         </div>
     );
 }
