4cd1609f9c98c285dbcfef5747bf6aa287c32f14
kota <kota200016@icloud.com>
2021-08-24 18:51:50 +0900

    各投稿記事へのリンク追加

--- src/components/commons/atoms/PostItem/Presenter.jsx
+++ src/components/commons/atoms/PostItem/Presenter.jsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 import styles from "./styles.module.scss";
 
 export function Presenter(props) {
     const { allPostsData } = props;
-    console.log(allPostsData);
+
     return (
         <>
             {allPostsData.map((postData) => {
@@ -11,12 +13,11 @@ export function Presenter(props) {
                         <time className={styles.post_time_stamp}>
                             {postData.date}
                         </time>
-                        <a
-                            href="https://www.u-nagano.ac.jp/"
-                            className={styles.post_title}
-                        >
-                            {postData.title}
-                        </a>
+                        <Link href={`/posts/${postData.id}`}>
+                            <a className={styles.post_title}>
+                                {postData.title}
+                            </a>
+                        </Link>
                     </li>
                 );
             })}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,6 @@ import { getSortedPostsData } from "../lib/posts";
 import { TopTemplate } from "../components/pages/TopTemplate";
 
 export default function Home({ allPostsData }) {
-    // console.log(allPostsData[0].id);
     return <TopTemplate allPostsData={allPostsData} />;
 }
 
